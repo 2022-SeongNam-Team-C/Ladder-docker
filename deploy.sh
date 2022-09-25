3c083e85810fc4bb5deaaf2ce3330a525aaabe6b
chan-han <hch99000@gmail.com>
2022-09-25 22:53:57 +0900

    워크플로우 환경 변수 잡 추가

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
  echo "> FE 배포"
 
- sudo docker-compose -f /home/Ladder/docker-compose.yml up -d --build
+sudo mkdir /home/test
+
+sudo docker-compose -f /home/Ladder/docker-compose.yml up -d --build
